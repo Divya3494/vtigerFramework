8f5fb485ec7f027e968ec58b856d2ab62b21c80c
prave <prave@LAPTOP-7TR7NGPB>
2023-08-23 12:59:04 +1000

    commit

---
+++ mavenpractice/src/test/java/mavenpracticepakage/DemoClasstest.java
@@ -0,0 +1,35 @@
+package mavenpracticepakage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DemoClasstest {
+
+	public void d()
+	{
+		WebDriver driver;
+		System.out.println("hiiii");
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		
+		if(BROWSER.equalsIgnoreCase("chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			 driver = new ChromeDriver();
+		}
+		else
+			if(BROWSER.equalsIgnoreCase("firefox"))
+			{
+				WebDriverManager.firefoxdriver().setup();
+				 driver = new FirefoxDriver();
+			}
+			else
+			{
+				driver=new ChromeDriver();
+			}
+		driver.get(URL);
+	}
+}
---
+++ mavenpractice/src/test/java/mavenpracticepakage/MavenPracticeTest.java
@@ -0,0 +1,18 @@
+package mavenpracticepakage;
+
+import org.testng.annotations.Test;
+
+public class MavenPracticeTest {
+
+	@Test
+	public void hi()
+	{
+		System.out.println("hiiii");
+	}
+	
+	@Test
+	public void hello()
+	{
+		System.out.println("hellooooooooo");
+	}
+}
