c91087342ddd03d321a817b6ba5982c9c4919eae
Divya3494 <142957122+Divya3494@users.noreply.github.com>
2023-08-23 13:07:37 +1000

    Update DemoClasstest.java

--- mavenpractice/src/test/java/mavenpracticepakage/DemoClasstest.java
+++ mavenpractice/src/test/java/mavenpracticepakage/DemoClasstest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DemoClasstest {
-
+//hii
 	public void d()
 	{
 		WebDriver driver;
